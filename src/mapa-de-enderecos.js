39759a66819159e89877398ac86ca3843ce00967
brenoov <breno@rbvcompany.com>
2026-07-04 11:55:51 -0300

    chore(vue): padroniza nomes dos componentes de tela para tela-de-<ferramenta>.vue (+ imports do roteador e LEIA-MEs)

--- src/mapa-de-enderecos.js
+++ src/mapa-de-enderecos.js
@@ -2,18 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { estado } from './compartilhado/controle-de-login-e-usuario.js'
 
 const rotas = [
-  { path: '/', name: 'inicio', component: () => import('./ferramentas/inicio/tela-inicial.vue') },
+  { path: '/', name: 'inicio', component: () => import('./ferramentas/inicio/tela-de-inicio.vue') },
   { path: '/noticias', name: 'noticias', component: () => import('./ferramentas/noticias/tela-de-noticias.vue') },
   { path: '/acessos', name: 'acessos', component: () => import('./ferramentas/acessos/tela-de-acessos.vue') },
   { path: '/banco', name: 'banco', component: () => import('./ferramentas/banco/tela-de-banco.vue') },
-  { path: '/vendas', name: 'vendas', component: () => import('./ferramentas/vendas/tela-menu-vendas.vue') },
-  { path: '/gestao-vista', name: 'gestao-vista', component: () => import('./ferramentas/gestao-a-vista/tela-gestao-a-vista.vue') },
-  { path: '/meta-ads', name: 'meta-ads', component: () => import('./ferramentas/meta-ads/tela-menu-meta-ads.vue') },
-  { path: '/meta-campanhas', name: 'meta-campanhas', component: () => import('./ferramentas/analise-campanhas/tela-analise-campanhas.vue') },
-  { path: '/gestao-trafego', name: 'gestao-trafego', component: () => import('./ferramentas/gestao-trafego/tela-gestao-trafego.vue') },
-  { path: '/gestao-comercial', name: 'gestao-comercial', component: () => import('./ferramentas/gestao-comercial/tela-gestao-comercial.vue') },
+  { path: '/vendas', name: 'vendas', component: () => import('./ferramentas/vendas/tela-de-menu-vendas.vue') },
+  { path: '/gestao-vista', name: 'gestao-vista', component: () => import('./ferramentas/gestao-a-vista/tela-de-gestao-a-vista.vue') },
+  { path: '/meta-ads', name: 'meta-ads', component: () => import('./ferramentas/meta-ads/tela-de-menu-meta-ads.vue') },
+  { path: '/meta-campanhas', name: 'meta-campanhas', component: () => import('./ferramentas/analise-campanhas/tela-de-analise-campanhas.vue') },
+  { path: '/gestao-trafego', name: 'gestao-trafego', component: () => import('./ferramentas/gestao-trafego/tela-de-gestao-trafego.vue') },
+  { path: '/gestao-comercial', name: 'gestao-comercial', component: () => import('./ferramentas/gestao-comercial/tela-de-gestao-comercial.vue') },
   { path: '/login', name: 'login', component: () => import('./ferramentas/login/tela-de-login.vue') },
-  { path: '/redes-sociais', name: 'redes-sociais', component: () => import('./ferramentas/redes-sociais/tela-redes-sociais.vue') },
+  { path: '/redes-sociais', name: 'redes-sociais', component: () => import('./ferramentas/redes-sociais/tela-de-redes-sociais.vue') },
 ]
 
 export const roteador = createRouter({
